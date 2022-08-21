68e65494b53b42d548070ac5a73c386b505279f6
imunokyh <dbsgh0180@gmail.com>
2022-08-21 16:12:24 +0900

    LOGIN

---
+++ 바닐라 JS로 크롬 앱 만들기/app.js
@@ -0,0 +1,34 @@
+// const loginForm = document.querySelector("#login-form");
+// const loginInput = document.querySelector("#login-form input");
+
+const loginForm = document.getElementById("login-form");
+const loginInput = loginForm.querySelector("input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASENAME = "hidden";
+const USERNAME_KEY = "username";
+
+function onLoginSubmit(event) {
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASENAME);
+    const username = loginInput.value;
+    console.log(username);
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreeting(username);
+}
+
+function paintGreeting(username) {
+    greeting.classList.remove(HIDDEN_CLASENAME);
+    greeting.innerText = "Hello " + username;
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+    // show the form
+    loginForm.classList.remove(HIDDEN_CLASENAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    // show the greetings
+    paintGreeting(savedUsername);
+}
